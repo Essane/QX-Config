d19c4d7e190a337dd44fef09f13738f4acba0390
Essane <33148228+Essane@users.noreply.github.com>
2022-08-07 22:58:07 +0800

    init

--- Script/ppgjx.js
+++ Script/ppgjx.js
@@ -6,4 +6,4 @@
 var body = JSON.parse($response.body);
 body.data.userInfo.userVip.isVip = 1;
 body.data.userInfo.userVip.endTime = "2099-07-16 10:04:32";
-$done({ body: JSON.stringify(obj) });
+$done({ body: JSON.stringify(body) });
